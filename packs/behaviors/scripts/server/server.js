02b6a6027ac79f62bbd182d911b441ea0be509c4
Roman Kravanja <roman.m.kravanja@student.fh-kiel.de>
2019-08-07 23:48:43 +0200

    The Pickaxe now has a texture, is craftable, has a name and its usage gets detected.

--- packs/behaviors/scripts/server/server.js
+++ packs/behaviors/scripts/server/server.js
@@ -3,19 +3,19 @@ var serverSystem = server.registerSystem(0, 0);
 // Setup which events to listen for
 serverSystem.initialize = function () {
     // set up your listenToEvents and register server-side components here.
-    //serverSystem.listenForEvent("allinonetemplate:pinky", receivePinkyMessage);
-    this.listenForEvent("minecraft:block_destruction_started", (eventData) => this.onPlayerBreakingBlock(eventData)); 
+    //serverSystem.listenForEvent("explodingpickaxeaddon:pinky", receivePinkyMessage);
+    this.listenForEvent("minecraft:block_destruction_started", (eventData) => this.onPlayerBreakingBlock(eventData));
 }
-let tickcount=0;
+let tickcount = 0;
 // per-tick updates
 serverSystem.update = function () {
     // Any logic that needs to happen every tick on the server.
     // Any logic that needs to happen every tick on the client.
-    if(tickcount > 80){
+    if (tickcount > 80) {
         message("Hello Client")
         tickcount = 0;
-    }else{
-        tickcount ++;
+    } else {
+        tickcount++;
     }
 
 }
@@ -23,13 +23,16 @@ serverSystem.update = function () {
 /**
  * whenever a player starts breaking a block, check the item 
  */
-serverSystem.onPlayerBreakingBlock = function(eventData){
-    message("Player "+ JSON.stringify(eventData.data["player"]) + " started breaking " + JSON.stringify(eventData.data["block_position"]))
+serverSystem.onPlayerBreakingBlock = function (eventData) {
+    //message("Player "+ JSON.stringify(eventData.data["player"]) + " started breaking " + JSON.stringify(eventData.data["block_position"]))
+    let usedItem = this.getComponent(eventData.data.player, "minecraft:hand_container").data[0];
+    if (usedItem["__identifier__"] == "explodingpickaxeaddon:explodingpickaxe") {
+        message("Yout used the thing at " + JSON.stringify(eventData.data["block_position"]))
+    }
 }
 
-function message(message)
-{
+function message(message) {
     let eventData = serverSystem.createEventData("minecraft:display_chat_event");
     eventData.data.message = message;
-    serverSystem.broadcastEvent("minecraft:display_chat_event",eventData);
+    serverSystem.broadcastEvent("minecraft:display_chat_event", eventData);
 }
